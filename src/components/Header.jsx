344b0234db8ae7481e8fb946232e2eeabbf39bf4
HAKD@1 <dubeyad2003@gmail.com>
2023-06-20 11:54:53 +0530

    adding spinner and fixing the bugs

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -19,6 +19,12 @@ export default function Header() {
     }
     return "text-gray-400 border-b-transparent";
   }
+  function pathMatchRoutes(route1, route2) {
+    if (route1 === location.pathname || route2 === location.pathname) {
+      return "text-black border-b-red-500";
+    }
+    return "text-gray-400 border-b-transparent";
+  }
   return (
     <div className="bg-white border-b shadow-sm sticky top-0 z-50">
       <header className="flex justify-between items-center px-3 max-w-6xl mx-auto">
@@ -49,9 +55,10 @@ export default function Header() {
               Offers
             </li>
             <li
-              className={`cursor-pointer py-3 text-sm font-semibold border-b-[3px] ${
-                pathMatchRoute("/sign-in") || pathMatchRoute("/profile")
-              }`}
+              className={`cursor-pointer py-3 text-sm font-semibold border-b-[3px] ${pathMatchRoutes(
+                "/profile",
+                "/sign-in"
+              )}`}
               onClick={() => navigate("/profile")}
             >
               {pageState}
