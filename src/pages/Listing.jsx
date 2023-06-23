9b3345618bb7ff4c885fd15ba28b4cd4cf42d083
HAKD@1 <dubeyad2003@gmail.com>
2023-06-23 13:01:38 +0530

    Added copy link to clipboard functionality

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -11,9 +11,11 @@ import SwiperCore, {
   Pagination,
 } from "swiper";
 import "swiper/css/bundle";
+import { FaShare } from "react-icons/fa";
 export default function Listing() {
   const [listing, setListing] = useState(null);
   const [loading, setLoading] = useState(true);
+  const [shareLinkCopied, setShareLinkCopied] = useState(false);
   SwiperCore.use([Autoplay, Navigation, Pagination]);
   const params = useParams();
   useEffect(() => {
@@ -36,7 +38,7 @@ export default function Listing() {
         pagination={{ type: "progressbar" }}
         effect="fade"
         modules={[EffectFade]}
-        autoplay={{ delay: 300 }}
+        autoplay={{ delay: 3000 }}
       >
         {listing.imgUrls.map((url, idx) => (
           <SwiperSlide key={idx}>
@@ -50,6 +52,23 @@ export default function Listing() {
           </SwiperSlide>
         ))}
       </Swiper>
+      <div
+        className="fixed top-[10%] right-[3%] z-10 bg-white cursor-pointer border-2 border-gray-400 rounded-full w-12 h-12 flex justify-center items-center"
+        onClick={() => {
+          navigator.clipboard.writeText(window.location.href);
+          setShareLinkCopied(true);
+          setTimeout(() => {
+            setShareLinkCopied(false);
+          }, 2000);
+        }}
+      >
+        <FaShare className="text-2xl text-slate-500" />
+      </div>
+      {shareLinkCopied && (
+        <p className="fixed top-[18%] right-[3%] font-semibold border-2 border-gray-400 rounded-md p-2 bg-white z-10">
+          Link copied to clipboard
+        </p>
+      )}
     </main>
   );
 }
